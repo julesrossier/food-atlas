783205b556164dcdaa92bcc708f00245c70d793c
Jules <jules.rossier@mailo.eu>
2025-12-11 17:13:32 +0100

    firt commit

---
+++ src/main/java/ch/heigvd/Main.java
@@ -0,0 +1,34 @@
+package ch.heigvd;
+
+import io.javalin.Javalin;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main {
+  public static final int PORT = 8080;
+
+  public static void main(String[] args) {
+    Javalin app = Javalin.create();
+
+    // This will serve as our database
+    Map<Integer, User> users = new ConcurrentHashMap<>();
+
+    // Controllers
+    AuthController authController = new AuthController(users);
+    UsersController usersController = new UsersController(users);
+
+    // Auth routes
+    app.post("/login", authController::login);
+    app.post("/logout", authController::logout);
+    app.get("/profile", authController::profile);
+
+    // Users routes
+    app.post("/users", usersController::create);
+    app.get("/users", usersController::getMany);
+    app.get("/users/{id}", usersController::getOne);
+    app.put("/users/{id}", usersController::update);
+    app.delete("/users/{id}", usersController::delete);
+
+    app.start(PORT);
+  }
+}
