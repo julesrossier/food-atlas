8b3a57fbf8ce7d547ba1fe3e6c28ace0ca644a70
yanniSkawronski <yanni.skawronski@heig-vd.ch>
2026-01-05 22:51:49 +0100

    Populate with realistic datas

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -21,11 +21,17 @@ public class Main {
     RecipeController recipeController = new RecipeController(recipeRepository, countryRepository);
     CountryController countryController = new CountryController(countryRepository);
 
-      recipeRepository.newRecipe(new Recipe("Test", 30, "AAAAAAAAAAAAAA", List.of("a", "b", "c")));
-      recipeRepository.newRecipe(new Recipe("Test2", 40, "AAAAAAAAAAAAA", List.of("a", "b", "c")));
-      recipeRepository.newRecipe(new Recipe("test3", 50, "RHDHDFHDFHFDFH", List.of("b", "c", "d")));
+    recipeRepository.newRecipe(new Recipe("Spaghetti carbonara", 30, "Sauce is composed with eggs, pancetta and parmesan cheese", List.of("main dish")));
+    recipeRepository.newRecipe(new Recipe("Fondue moitié-moitié", 25, "Composed of Vacherin Fribourgeois and Gruyère AOP", List.of("vegetarian", "alcohol")));
+    recipeRepository.newRecipe(new Recipe("Taboulé", 25, "The traditional delicious recipe", List.of("vegetarian", "vegan", "salad")));
+    recipeRepository.newRecipe(new Recipe("Rösti", 50, "Can be served with fried egg on top", List.of("vegetarian", "gluten free")));
+    recipeRepository.newRecipe(new Recipe("Ratatouille", 80, "Mix of delicious vegetables", List.of("vegetarian", "vegan", "gluten free")));
+    recipeRepository.newRecipe(new Recipe("Omelette", 10, "Beaten eggs cooked in a pan", List.of("vegetarian")));
 
-      countryRepository.newCountry(new Country("CHE", "Switzerland", List.of(1)));
+    countryRepository.newCountry(new Country("CHE", "Switzerland", List.of(2, 4)));
+    countryRepository.newCountry(new Country("ITA", "Italy", List.of(1)));
+    countryRepository.newCountry(new Country("LBN", "Lebanon", List.of(3)));
+    countryRepository.newCountry(new Country("FRA", "France"));
 
     app.get("/recipes", recipeController::getRecipes);
     app.post("/recipes", recipeController::addRecipe);
